777001638140a29f54f7429d9fe8bad3aa58d697
Shemistan <shemistan1@gmail.com>
2021-03-04 15:22:07 +0300

    version1

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+
+
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yatube.settings')
     try:
@@ -13,5 +15,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+    
 if __name__ == '__main__':
     main()
