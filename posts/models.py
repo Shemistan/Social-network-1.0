2478f817e7acca53db2be43d0c8662530e8230b3
Shemistan <shemistan1@gmail.com>
2021-03-04 15:05:29 +0300

    version1

---
+++ posts/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField("date published", auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts") 
+    group = models.ForeignKey('Group', models.SET_NULL, blank=True, null=True, related_name="posts")
+
+    def __str__(self):
+        return self.author
+
+class Group(models.Model):
+    title = models.CharField('Group name', max_length=200)
+    slug = models.SlugField('Url', max_length=200, unique=True)
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.title
+
+
