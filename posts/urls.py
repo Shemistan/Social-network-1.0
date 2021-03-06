ba7660f4d5a2a25ee4794e5acd99192b6f3dd6e5
Shemistan <shemistan1@gmail.com>
2021-03-06 11:29:34 +0300

    version1

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("group/<slug>/", views.group_posts, name="group_posts")
+    path("group/<slug:slug>/", views.group_posts, name="group_posts")
 ]
