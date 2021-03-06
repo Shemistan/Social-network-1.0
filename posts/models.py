e0c993af8d36a00b5f53b25d0be9169df304c9ae
Shemistan <shemistan1@gmail.com>
2021-03-06 14:18:07 +0300

    version1

--- posts/models.py
+++ posts/models.py
@@ -12,6 +12,9 @@ class Post(models.Model):
     group = models.ForeignKey("Group", models.SET_NULL, blank=True,
                               null=True, related_name="posts")
 
+    class Meta:
+        ordering = ["-pub_date"]
+
     def __str__(self):
         return self.author
 
