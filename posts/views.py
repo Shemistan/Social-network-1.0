4c8801d00f538ce159b8496c4f307e41b55d4abe
Shemistan <shemistan1@gmail.com>
2021-03-07 13:39:10 +0300

    version2

--- posts/views.py
+++ posts/views.py
@@ -9,5 +9,5 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.order_by("-pub_date")[:12]
+    posts = group.posts.all()[:12]
     return render(request, "group.html", {"group": group, "posts": posts})
